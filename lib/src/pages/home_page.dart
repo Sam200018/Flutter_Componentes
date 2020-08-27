74f78a2946e0075270286b6518201882a069bb33
Sam Bautista <samuelbaubas@gmail.com>
2020-08-26 23:06:14 -0500

    Se usa el home_page y cambiamos el archivo para empezar a leer un archivo JSON

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componetes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    return ListView(
+      children: _listaItems(),
+    );
+  }
+
+  List<Widget> _listaItems() {
+    return [
+      ListTile(
+        title: Text("Hola MUndo"),
+      ),
+      Divider(),
+    ];
+  }
+}

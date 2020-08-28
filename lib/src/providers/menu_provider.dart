c05ec0524bf33445ac3e96727bac814afda038f5
Sam Bautista <samuelbaubas@gmail.com>
2020-08-28 10:59:56 -0500

    Estamos recorriendo la dara y colocando un texto con lo que nos llega en el future builder

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -5,14 +5,16 @@ import 'dart:convert';
 class _MenuProvider {
   List<dynamic> opciones = [];
 
-  _MenuProvider() {}
+  _MenuProvider() {
+    //hola
+  }
   Future<List<dynamic>> cargarData() async {
     //El rootBundle es solo para poder cargar el json y solo poder mostrarlo en consola
-    final resp = await rootBundle
-        .loadString('data/menu_opts.json'); //asiganos a una variable final
+    final resp = await rootBundle.loadString("data/menu_opts.json");
+    //asiganos a una variable final
 
     Map dataMap = json.decode(resp); //esa variable final rep la decodificamos
-    print(dataMap['rutas']);
+    // print(dataMap['rutas']);
     opciones = dataMap['rutas']; //y la decodificación la ponemos en opciones
 
     return opciones; // regremos opciones siendo un future builder y que va a regresar la información de manera asincrona

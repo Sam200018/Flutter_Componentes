de56b3914441e6a8acb8cceded7d6ee936fe61b5
Sam Bautista <samuelbaubas@gmail.com>
2020-09-02 12:30:21 -0500

    email y password fields, es como cualquier input pero solo cambia un par de cosas

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -9,6 +9,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = '';
+  String _email = '';
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,6 +21,10 @@ class _InputPageState extends State<InputPage> {
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
           children: [
             _crearInput(),
+            Divider(),
+            _crearEmail(),
+            Divider(),
+            _crearPassword(),
             Divider(),
             _crearPersona(),
           ],
@@ -51,9 +56,50 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Pasword',
+        labelText: 'Pasword',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
   Widget _crearPersona() {
     return ListTile(
       title: Text('Nombre es $_nombre'),
+      subtitle: Text('Email es $_email'),
     );
   }
 }

2e1d600db5739202ee629cd602814b374add4e9b
Sam Bautista <samuelbaubas@gmail.com>
2020-09-03 19:10:57 -0500

    Cambiamos el datepicker de idioma, teniamos agregar un import en el main y ulos idiomas ahí mismo, de ahí agregar una dependencia en el pubspec.yaml y usar el parametro nombrado, el locale que recibe un Locale() con el idioma

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
@@ -11,6 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English, no country code
+        const Locale('es', 'ES'),
+      ],
       // home: HomePageTemp(),
       // home: HomePage(),
       initialRoute:
